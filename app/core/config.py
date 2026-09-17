5062280fc26c84d0c52be7f8d106395427c57093
Faizan Ahmad <faizan.ahmad12877@gmail.com>
2026-09-17 20:04:29 +0530

    allow frontend variables in backend environment

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,13 @@
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
     DATABASE_URL: str
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str = "HS256"
     GROQ_API_KEY: str
     REDIS_URL: str = "redis://localhost:6379"
-
-    class Config:
-        env_file = ".env"
 
 settings = Settings()
