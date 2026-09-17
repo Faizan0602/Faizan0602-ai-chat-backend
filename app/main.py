31677d2814901fd94b549d1d4fb090d59eb4ef0c
Faizan Ahmad <faizan.ahmad12877@gmail.com>
2026-09-17 18:25:48 +0530

    cors configured

--- app/main.py
+++ app/main.py
@@ -6,14 +6,20 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from fastapi.middleware.cors import CORSMiddleware
+import os
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="AI Chat Backend")
 
+
+    
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
