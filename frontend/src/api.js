e14fe734a8dd59d9de6f991a928c53dbbc58b9bc
Faizan Ahmad <faizan.ahmad12877@gmail.com>
2026-09-17 19:45:17 +0530

    hardcode production api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL;
+const API_BASE = "https://orbitchat-b3ku.onrender.com/api/v1";
 
 const api = axios.create({ baseURL: API_BASE });
 let refreshPromise = null;
